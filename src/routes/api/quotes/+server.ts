7e11948807b3f56c5458f29710c10af77970b1e9
Sven Hansen <76601644+HansenSven@users.noreply.github.com>
2023-11-29 12:10:05 +0100

    Add temporary get all quotes

--- src/routes/api/quotes/+server.ts
+++ src/routes/api/quotes/+server.ts
@@ -1,5 +1,5 @@
 import db from '$lib/database';
-import { error, type RequestEvent, type RequestHandler } from "@sveltejs/kit";
+import type {error, RequestEvent, RequestHandler} from "@sveltejs/kit";
 
 const responseHeaders = {
   headers: {
@@ -8,43 +8,44 @@ const responseHeaders = {
 };
 
 export const GET: RequestHandler = async (reqEvent: RequestEvent) => {
-  try {
-    const page = reqEvent.url.searchParams.get('page');
-    let quotes;
-
-    if (page) {
-      const pageNumber = parseInt(page, 10);
-      quotes = await db.quote.findMany({
-        skip: (pageNumber - 1) * 10,
-        take: 10,
-        orderBy: {
-          id: 'asc',
-        },
-        include: {
-          votes: true,
-        }
-      });
-    } else {
-      const quotesWithVotes = await db.quote.findMany({
-        where: {
-          votes: {
-            some: {},
-          },
-        },
-        include: {
-          votes: true,
-        },
-      });
-
-      const filteredQuotes = quotesWithVotes.filter((quote) => quote.votes.length >= 3);
-      filteredQuotes.sort((a, b) => b.votes.length - a.votes.length);
-      quotes = filteredQuotes.slice(0, 20);
-    }
-
-    return new Response(JSON.stringify(quotes), responseHeaders);
-  } catch (e) {
-    throw error(500, 'Failed to fetch quotes.');
-  }
+  return new Response(JSON.stringify(await db.quote.findMany({ include: { votes: true}})), responseHeaders);
+  // try {
+  //   const page = reqEvent.url.searchParams.get('page');
+  //   let quotes;
+  //
+  //   if (page) {
+  //     const pageNumber = parseInt(page, 10);
+  //     quotes = await db.quote.findMany({
+  //       skip: (pageNumber - 1) * 10,
+  //       take: 10,
+  //       orderBy: {
+  //         id: 'asc',
+  //       },
+  //       include: {
+  //         votes: true,
+  //       }
+  //     });
+  //   } else {
+  //     const quotesWithVotes = await db.quote.findMany({
+  //       where: {
+  //         votes: {
+  //           some: {},
+  //         },
+  //       },
+  //       include: {
+  //         votes: true,
+  //       },
+  //     });
+  //
+  //     const filteredQuotes = quotesWithVotes.filter((quote) => quote.votes.length >= 3);
+  //     filteredQuotes.sort((a, b) => b.votes.length - a.votes.length);
+  //     quotes = filteredQuotes.slice(0, 20);
+  //   }
+  //
+  //   return new Response(JSON.stringify(quotes), responseHeaders);
+  // } catch (e) {
+  //   throw error(500, 'Failed to fetch quotes.');
+  // }
 };
 
 export const DELETE: RequestHandler = async (reqEvent: RequestEvent) => {
