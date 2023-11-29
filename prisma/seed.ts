dbf3f885c252beae6c569fa68d9148bfbbb82f54
Jelle Maas <typiqally@gmail.com>
2023-11-29 16:55:47 +0100

    Fix seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,39 +3,49 @@ import {faker} from '@faker-js/faker';
 import db from '$lib/database';
 
 const generateQuotes = async (count: number) => {
-  const quotes = [];
-  for (let i = 0; i < count; i++) {
-    const newQuote = await db.quote.create({
-      data: {
-        text: faker.lorem.sentence(),
-        discordId: faker.number.int({min: 1000, max: 100000000}).toString(),
-        createdAt: faker.date.past(),
-      },
-    });
-    quotes.push(newQuote);
-  }
-  return quotes;
+    const quotes = [];
+
+    for (let i = 0; i < count; i++) {
+        const newQuote = await db.quote.create({
+            data: {
+                text: faker.lorem.sentence(),
+                discordId: faker.number.int({min: 1000, max: 100000000}).toString(),
+                createdAt: faker.date.past(),
+            },
+        });
+
+        quotes.push(newQuote);
+    }
+
+    return quotes;
 };
 
-const generateVotes = async (quotes: Quote[], voteCountPerQuote: number) => {
-  const votes = [];
-  for (const quote of quotes) {
-    for (let i = 0; i < voteCountPerQuote; i++) {
-      const newVote = await db.vote.create({
-        data: {
-          discordId: faker.number.int({min: 1000, max: 100000000}).toString(),
-          createdAt: faker.date.past(),
-          quote: { connect: { id: quote.id } },
-        },
-      });
-      votes.push(newVote);
+const generateVotes = async (quote: Quote, count: number) => {
+    const votes = [];
+
+    for (let i = 0; i < count; i++) {
+        const vote = await db.vote.create({
+            data: {
+                discordId: faker.number.int({min: 1000, max: 100000000}).toString(),
+                createdAt: faker.date.past(),
+                quote: {connect: {id: quote.id}},
+            },
+        });
+
+        votes.push(vote)
     }
-  }
-  return votes;
+
+    return votes
 };
 
 generateQuotes(10)
-    .then((generatedQuotes) => {
-      const voteCountPerQuote = Math.floor(Math.random() * 10) + 1;
-      return generateVotes(generatedQuotes, voteCountPerQuote);
+    .then(async (generatedQuotes) => {
+        const votes = [];
+
+        for (const quote of generatedQuotes) {
+            const count = Math.floor(Math.random() * 10) + 1;
+            votes.push(await generateVotes(quote, count))
+        }
+
+        return votes
     })
