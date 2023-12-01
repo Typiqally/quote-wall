62929a8e2a7e08a51dc86d88d1f7dad616dd67da
Jelle Maas <typiqally@gmail.com>
2023-12-01 12:20:30 +0100

    Tweak parameters

--- src/routes/api/quotes/top/+server.ts
+++ src/routes/api/quotes/top/+server.ts
@@ -3,17 +3,13 @@ import db, {jsonResponseHeaders} from "$lib/database";
 
 export const GET: RequestHandler = async (reqEvent: RequestEvent) => {
     const quotesWithVotes = await db.quote.findMany({
-        where: {
-            votes: {
-                some: {},
-            },
-        },
         include: {
             votes: true,
         },
     });
 
-    const filteredQuotes = quotesWithVotes.filter((quote) => quote.votes.length >= 3);
+    const minimumVotes = 0
+    const filteredQuotes = quotesWithVotes.filter((quote) => quote.votes.length >= minimumVotes);
     filteredQuotes.sort((a, b) => b.votes.length - a.votes.length);
 
     const quotes = filteredQuotes.slice(0, 20);
